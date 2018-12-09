2fcb443c23dc355c74c87239c1544b2aa2754db5
Benjamin Barre <benjamin@cardiologs.com>
2018-12-09 17:05:58 +0100

    initial commit

---
+++ src/views/Realtime.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="">
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+import TimelinePlot from '@/components/TimelinePlot.vue';
+
+@Component({
+  components: {
+    TimelinePlot,
+  },
+})
+export default class RealTime extends Vue {}
+</script>
